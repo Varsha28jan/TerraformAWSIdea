af811f65bfab765be6e736187b87fb1bea9dba68
Varsha Patil <47296386+Varsha28jan@users.noreply.github.com>
2023-10-27 17:11:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -48,3 +48,83 @@ variable "iam_instance_profile_name" {
   type    = string
   default = ""
 }
+
+
+
+#vpc
+variable "vpc_name" {
+  description = "Name of the vpc"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_availability_zones" {
+  description = "A list of availability zones"
+  type        = list(string)
+  default     = [""]
+}
+
+variable "vpc_public_subnets_cidr" {
+  description = "A list of public subnets "
+  type        = list(string)
+  default     = [""]
+}
+
+variable "vpc_private_subnets_cidr" {
+  description = "A list of private subnets "
+  type        = list(string)
+  default     = [""]
+}
+
+variable "vpc_database_subnets_cidr" {
+  description = "A list of database subnets "
+  type        = list(string)
+  default     = [""]
+}
+
+variable "reuse_nat_ips" {
+  type    = bool
+  default = false
+}
+
+variable "external_natip_id" {
+  type    = list(string)
+  default = [""]
+}
+
+variable "vpc_public_subnet_tags_name" {
+  description = "Name of the subnet_tags"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_private_subnet_tags_name" {
+  description = "Name of the subnet_tags"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_database_subnet_tags_name" {
+  description = "Name of the subnet_tags"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_environment" {
+  description = "type of the environment"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_region" {
+  description = "aws region"
+  type        = string
+  default     = ""
+}
+#end of vpc
